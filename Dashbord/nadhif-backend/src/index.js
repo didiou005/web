43b170b36b232a24460c6e8a38127040c898dd64
Walid Ouadah <ouadahwal@gmail.com>
2026-05-07 18:22:50 +0100

    Update index.js

--- Dashbord/nadhif-backend/src/index.js
+++ Dashbord/nadhif-backend/src/index.js
@@ -5,7 +5,22 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://web-ekfg.vercel.app',
+  ],
+
+  methods: [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS'
+  ],
+
+  credentials: true,
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
