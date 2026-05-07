8974930e4a54cbb02694b9246ad31df79bc41f33
Walid Ouadah <ouadahwal@gmail.com>
2026-05-07 15:48:43 +0100

    Update FollowUp.jsx

--- client/src/FollowUp.jsx
+++ client/src/FollowUp.jsx
@@ -1,4 +1,5 @@
 ﻿import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import { useLocation } from 'react-router-dom';
 import { useLanguage } from './LanguageContext';
 import Skeleton from './Skeleton';
