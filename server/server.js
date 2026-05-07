2b6788b570b939b43fbbf5422e1bdd92f11c26d6
Walid Ouadah <ouadahwal@gmail.com>
2026-05-07 15:06:29 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+
 
 /* =========================================================
    DATABASE
@@ -404,8 +404,8 @@ pool.query("SELECT NOW()")
    START SERVER
 ========================================================= */
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(
-    `🚀 Serveur démarré sur le port ${PORT}`
-  );
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur démarré sur le port ${PORT}`);
 });
