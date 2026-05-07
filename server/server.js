48ab113e06bf95ca10d7018194d14e083b8acd1d
Walid Ouadah <ouadahwal@gmail.com>
2026-05-07 14:37:51 +0100

    db

--- server/server.js
+++ server/server.js
@@ -16,12 +16,10 @@ const PORT = process.env.PORT || 3000;
 ========================================================= */
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 /* =========================================================
