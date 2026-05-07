92b2e4c22b67845168fd40a474bea9414d4b7fb2
Walid Ouadah <ouadahwal@gmail.com>
2026-05-07 15:25:53 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -172,8 +172,8 @@ app.get("/api/communes", async (req, res) => {
 
 app.post(
   "/api/complaints",
-  upload.array("photos", 5),
-
+  //upload.array("photos", 5),
+  (req, res, next) => next(),
   async (req, res) => {
     console.log("📥 POST /api/complaints reached");
 
